f88f1e2e5a61191e0c9b4f25b98ac1e72abeb799
Anca <ancuta.tatomir@gmail.com>
2021-01-08 20:44:15 +0200

    cars

---
+++ inmultire/puzzle/car/car.js
@@ -0,0 +1,37 @@
+let dacia = {
+
+    make: "Dacia",
+    model: 1310,
+    color: "verde",
+    year: 1978,
+    mileage: 526737,
+    maxSpeed: 180,
+    paint: function(color) {
+    this.color = color
+
+
+    }
+}
+
+dacia.paint("galben")
+console.log(dacia)
+
+
+
+
+let ferrari = {
+  
+    make: "Ferrari",
+    model: 1310,
+    color: "rosu",
+    year: 1972,
+    mileage: 526737,
+    maxSpeed: 180,
+    paint: function(color) {
+    this.color = color }
+
+
+}
+
+
+console.log(ferrari)
---
+++ inmultire/puzzle/puzzle.js
@@ -0,0 +1,18 @@
+
+
+for (let w = 0; w <=11; w++) {
+    document.write("<br>") 
+    for (let h =0; h <= 21; i++)  {
+        if((w==0)|| (w==11)||(h==0)||(h==21))
+        
+        {
+           document.write("#")
+       }else{
+           document.write("0") }        
+       }
+}
+
+
+
+
+
