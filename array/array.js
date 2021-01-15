547d437ee3fc7f8a68238a13f5f82cbf7ab6ec51
Anca <ancuta.tatomir@gmail.com>
2021-01-15 20:46:20 +0200

    balonase

---
+++ array/array.js
@@ -0,0 +1,15 @@
+let bubbles = [60, 59, 48, 71, 23, 17, 10, 8, 56, 79]
+
+
+let fruits = ["apples", "mango", "apple" , "oranges" , "cucumber"] 
+
+
+console.log(bubbles.sort())
+max = bubbles.pop()
+
+
+fruits.sort()
+fruits.reverse()
+
+console.log(max)
+console.log(fruits)
