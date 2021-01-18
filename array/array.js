526f6fbc4bfdea7fd77fdfee4d234ed16c2ea058
Anca <ancuta.tatomir@gmail.com>
2021-01-18 20:47:06 +0200

    arrays

--- array/array.js
+++ array/array.js
@@ -12,5 +12,42 @@ max = bubbles.pop()
 fruits.sort()
 fruits.reverse()
 
+
+bubbles[0]
+fruits[2]
+
 console.log(max)
 console.log(fruits)
+
+
+for (let index = 0; index < fruits.length; index++) {
+    const fruit = fruits[index];
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+    
+}
+
+for (const fruit of fruits) {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+    
+    
+}
+
+fruits.forEach(fruit => {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)
+    
+});
+
+// toate variantele de mai sus sunt similare
+
+for (const fruit in fruits) {
+    let listItem = document.createElement("li")
+    listItem.innerText = fruit
+    document.querySelector("ul").appendChild(listItem)     
+  
+}
--- bubbles/bubbles.js
+++ bubbles/bubbles.js
@@ -1,4 +1,4 @@
-var numberOfBubbles = 100
+var numberOfBubbles = 20
 for (let i = 0; i < numberOfBubbles; i++) {
     newBubble () 
 }
@@ -7,9 +7,21 @@ function newBubble() {
     let bubble = document.createElement("div")
     bubble.classList.add("bubble")
     let x = randomNumber(100)
-    let delay = randomNumber(2000)
-    bubble.style.left = x + "vw" 
-    bubble.style.animationDelay = delay + "ms"
+    let delay = randomNumber(3000)
+    let red = randomNumber(255)
+    let green = randomNumber(255)
+    let blue = randomNumber(255)
+    let size = randomNumber(3) + 1
+
+
+
+    // bubble.style.left = x + "vw"   
+    bubble.style.left = `${x}vw` 
+    bubble.style.animationDelay = `${delay}ms`
+   //  bubble.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")"
+    bubble.style.borderColor = `rgb(${red},${green},${blue})`
+    bubble.style.height = `${size}em`
+    bubble.style.width = `${size}em`
     document.querySelector("body").appendChild(bubble)
 
 }
