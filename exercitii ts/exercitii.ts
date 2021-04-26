349d5f71c7b3de40e8a90a96756cb077f05fa536
Anca <ancuta.tatomir@gmail.com>
2021-04-26 19:45:37 +0300

    vue

---
+++ exercitii ts/exercitii.ts
@@ -0,0 +1,23 @@
+interface Masina {
+    greutate: Number
+    kilometraj: Number
+    start() : void }
+
+
+class BMW implements Masina, Tehnic {
+    greutate: NumberStart ( ) {
+
+    }
+}
+
+class Dacia implements Masina {
+    greutate: Number 
+    start ( ) {
+
+
+    }
+
+}
+
+
+
